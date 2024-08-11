aeba25aaee60dcd859f010829f8447f85d70317a
Sumit Kumar <49060256+sumitchaudhary11@users.noreply.github.com>
2024-08-11 18:19:29 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  db_port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST, // Use environment variables for sensitive info
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306 // Default port if not specified
 
 });
 
