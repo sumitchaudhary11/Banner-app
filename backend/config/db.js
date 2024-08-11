3600351ea7727611f9255e8ed7b8b58effe1d704
Sumit Kumar <49060256+sumitchaudhary11@users.noreply.github.com>
2024-08-11 17:40:36 +0530

    Add .env file and update server.js and db.js configurations

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Sum@my1290",
-  database: "banner_app",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+
 });
 
 db.connect((err) => {
